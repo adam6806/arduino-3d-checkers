5e4517a9c46b8568a826dace71f3b2f706e3f817
Adam <asmith0935@gmail.com>
2013-07-24 16:00:16 -0600

    Mapped led's to xyz. Added effects.

--- BareTest/BareTest.ino
+++ BareTest/BareTest.ino
@@ -48,6 +48,7 @@ uint16_t ledNum = 40;
 WS2801 strip = WS2801(ledNum, dataPin, clockPin);
 int randomIndex;
 int randomColor;
+int cube[4][4][4];
 
 
 void setup() {
@@ -55,16 +56,119 @@ void setup() {
   Serial.begin(9600);
   strip.begin();
   strip.show();
-
+  setArray();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  strobe();
+  //strobe();
   //pulseLeds();
+  //inOrderCycle();
+  //cubeCycle();
+  cubeVertPlaneShift();
+  cubeHorizPlaneShift();
   
+}
+
+void cubeHorizPlaneShift() {
+  for(int i=0;i<4;i++) {
+    for(int j=0;j<4;j++) {
+      for(int k=0;k<4;k++) {
+        if(cube[j][k][i]!=32) {
+          changePix(cube[j][k][i], random(1,8));
+          refresh();
+        }
+      }
+    }
+    delay(200);
+    allOff();
+  }
+  allOff();
   
+}
+
+void cubeVertPlaneShift() {
+  for(int i=0;i<4;i++) {
+    for(int j=0;j<4;j++) {
+      for(int k=0;k<4;k++) {
+        if(cube[i][j][k]!=32) {
+          changePix(cube[i][k][j], random(1,8));
+          refresh();
+        }
+      }
+    }
+    delay(200);
+    allOff();
+  }
+  allOff();
+  
+}
+
+void cubeCycle() {
+  for(int i=0;i<4;i++) {
+    for(int j=0;j<4;j++) {
+      for(int k=0;k<4;k++) {
+        if(cube[k][j][i]!=32) {
+          changePix(cube[k][j][i], random(1,8));
+          refresh();
+          delay(100);
+        }
+      }
+    }
+  }
+  allOff();
+}
+
+void setArray() {
+  for(int i=0;i<4;i++) {
+    for(int j=0;j<4;j++) {
+      for(int k=0;k<4;k++) {
+        cube[i][j][k] = 32;
+      }
+    }
+  }
+  cube[0][0][0] = 0;
+  cube[0][2][0] = 1;
+  cube[1][3][0] = 2;
+  cube[1][1][0] = 3;
+  cube[2][0][0] = 4;
+  cube[2][2][0] = 5;
+  cube[3][3][0] = 6;
+  cube[3][1][0] = 7;
+  cube[3][0][1] = 8;
+  cube[3][2][1] = 9;
+  cube[2][3][1] = 10;
+  cube[2][1][1] = 11;
+  cube[1][0][1] = 12;
+  cube[1][2][1] = 13;
+  cube[0][3][1] = 14;
+  cube[0][1][1] = 15;
+  cube[0][0][2] = 16;
+  cube[2][0][2] = 17;
+  cube[3][1][2] = 18;
+  cube[1][1][2] = 19;
+  cube[0][2][2] = 20;
+  cube[2][2][2] = 21;
+  cube[3][3][2] = 22;
+  cube[1][3][2] = 23;
+  cube[0][3][3] = 24;
+  cube[0][1][3] = 25;
+  cube[1][2][3] = 26;
+  cube[1][0][3] = 27;
+  cube[2][1][3] = 28;
+  cube[2][3][3] = 29;
+  cube[3][2][3] = 30;
+  cube[3][0][3] = 31;
+}
+
+void inOrderCycle() {
+  for(int i=0;i<32;i++){
+    changePix(i,3);
+    refresh();
+    delay(500);
+    allOff();
+  }
 }
 
 void strobe() {
