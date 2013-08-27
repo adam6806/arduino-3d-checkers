6fddf97f8648b4a1f9caea18f9ecb1e7686dad28
Adam <asmith0935@gmail.com>
2013-08-27 21:49:39 +0000

    Fixed Mapping

--- BareTest/BareTest.ino
+++ BareTest/BareTest.ino
@@ -62,6 +62,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   
+  
+  
   for(int i=0;i<200;i++){
     strobe();
   }
@@ -70,7 +72,11 @@ void loop() {
   }
   
   inOrderCycle();
+  
+  
   cubeCycle();
+  
+  
   
   for(int k=0;k<3;k++){
     cubeVertPlaneShift();
@@ -82,14 +88,15 @@ void loop() {
   }
   
   
+  
 }
 
 void cubeHorizPlaneShift() {
   for(int i=0;i<4;i++) {
     for(int j=0;j<4;j++) {
       for(int k=0;k<4;k++) {
-        if(cube[j][k][i]!=32) {
-          changePix(cube[j][k][i], random(1,8));
+        if(cube[j][i][k]!=32) {
+          changePix(cube[j][i][k], random(1,8));
           refresh();
         }
       }
@@ -105,8 +112,8 @@ void cubeVertPlaneShift() {
   for(int i=0;i<4;i++) {
     for(int j=0;j<4;j++) {
       for(int k=0;k<4;k++) {
-        if(cube[i][j][k]!=32) {
-          changePix(cube[i][k][j], random(1,8));
+        if(cube[i][k][j]!=32) {
+          changePix(cube[i][j][k], random(1,8));
           refresh();
         }
       }
@@ -122,8 +129,8 @@ void cubeCycle() {
   for(int i=0;i<4;i++) {
     for(int j=0;j<4;j++) {
       for(int k=0;k<4;k++) {
-        if(cube[k][j][i]!=32) {
-          changePix(cube[k][j][i], random(1,8));
+        if(cube[k][i][j]!=32) {
+          changePix(cube[k][i][j], random(1,8));
           refresh();
           delay(300);
         }
@@ -137,42 +144,43 @@ void setArray() {
   for(int i=0;i<4;i++) {
     for(int j=0;j<4;j++) {
       for(int k=0;k<4;k++) {
-        cube[i][j][k] = 32;
+        cube[i][k][j] = 32;
       }
     }
   }
   cube[0][0][0] = 0;
-  cube[0][2][0] = 1;
-  cube[1][3][0] = 2;
-  cube[1][1][0] = 3;
+  cube[0][2][0] = 22;
+  cube[1][3][0] = 31;
+  cube[1][1][0] = 9;
   cube[2][0][0] = 4;
-  cube[2][2][0] = 5;
-  cube[3][3][0] = 6;
-  cube[3][1][0] = 7;
-  cube[3][0][1] = 8;
-  cube[3][2][1] = 9;
-  cube[2][3][1] = 10;
+  cube[2][2][0] = 18;
+  cube[3][3][0] = 30;
+  cube[3][1][0] = 8;
+  cube[3][0][1] = 7;
+  cube[3][2][1] = 17;
+  cube[2][3][1] = 29;
   cube[2][1][1] = 11;
-  cube[1][0][1] = 12;
-  cube[1][2][1] = 13;
-  cube[0][3][1] = 14;
-  cube[0][1][1] = 15;
-  cube[0][0][2] = 16;
-  cube[2][0][2] = 17;
-  cube[3][1][2] = 18;
-  cube[1][1][2] = 19;
-  cube[0][2][2] = 20;
-  cube[2][2][2] = 21;
-  cube[3][3][2] = 22;
-  cube[1][3][2] = 23;
+  cube[1][0][1] = 3;
+  cube[1][2][1] = 21;
+  cube[0][3][1] = 28;
+  cube[0][1][1] = 10;
+  cube[0][0][2] = 1;
+  cube[2][0][2] = 5;
+  cube[3][1][2] = 12;
+  cube[1][1][2] = 13;
+  cube[0][2][2] = 23;
+  cube[2][2][2] = 19;
+  cube[3][3][2] = 26;
+  cube[1][3][2] = 27;
   cube[0][3][3] = 24;
-  cube[0][1][3] = 25;
-  cube[1][2][3] = 26;
-  cube[1][0][3] = 27;
-  cube[2][1][3] = 28;
-  cube[2][3][3] = 29;
-  cube[3][2][3] = 30;
-  cube[3][0][3] = 31;
+  cube[0][1][3] = 14;
+  cube[1][2][3] = 20;
+  cube[1][0][3] = 2;
+  cube[2][1][3] = 15;
+  cube[2][3][3] = 25;
+  cube[3][2][3] = 16;
+  cube[3][0][3] = 6;
+  
 }
 
 void inOrderCycle() {
